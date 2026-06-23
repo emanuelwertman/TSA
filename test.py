0d60593adf5cf04d47a8774f20364aad9246b9d4
emanuelwertman <emanuel.wertman@gmail.com>
2026-06-23 01:25:38 -0400

    test9

--- test.py
+++ test.py
@@ -11,33 +11,56 @@ IN1 = 29
 IN2 = 31
 ENA = 32
 
+IN3 = 38
+IN4 = 40
+ENB = 33
+
 # Setup GPIO mode
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(IN1, GPIO.OUT)
 GPIO.setup(IN2, GPIO.OUT)
 GPIO.setup(ENA, GPIO.OUT)
 
+GPIO.setup(IN3, GPIO.OUT)
+GPIO.setup(IN4, GPIO.OUT)
+GPIO.setup(ENB, GPIO.OUT)
+
 # Initialize PWM on ENA pin at 100Hz frequency
-pwm = GPIO.PWM(ENA, 500)
-pwm.start(0)  # Start with 0% duty cycle (Off)
+pwm1 = GPIO.PWM(ENA, 500)
+pwm1.start(0)  # Start with 0% duty cycle (Off)
+pwm2 = GPIO.PWM(ENB, 500)
+pwm2.start(0)
 
 try:
     print("Pump On")
-    
-    # Set the speed to 100% (Duty cycle ranges from 0.0 to 100.0 in RPi.GPIO)
-    pwm.ChangeDutyCycle(100)
     
     # Establish direction: IN1 High and IN2 Low for forward
     GPIO.output(IN1, GPIO.HIGH)
     GPIO.output(IN2, GPIO.LOW)
     
+    pwm1.ChangeDutyCycle(100)
+
+    GPIO.output(IN3, GPIO.HIGH)
+    GPIO.output(IN4, GPIO.LOW)
+    pwm2.ChangeDutyCycle(100)
+
     # Run for 5 seconds
     sleep(5)
 
 finally:
     # Stop the pump and clean up the pins safely
     print("Done")
-    pwm.stop()
+
     GPIO.output(IN1, GPIO.LOW)
     GPIO.output(IN2, GPIO.LOW)
+    
+    pwm1.ChangeDutyCycle(0)
+
+    GPIO.output(IN3, GPIO.LOW)
+    GPIO.output(IN4, GPIO.LOW)
+    pwm2.ChangeDutyCycle(0)
+
+    pwm1.stop()
+    pwm2.stop()
+
     GPIO.cleanup()
